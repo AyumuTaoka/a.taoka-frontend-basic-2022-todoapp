f546059ece3d758480ab0776053478663afb4368
AyumuTaoka <a.taoka@4nonome.com>
2022-07-15 05:53:53 +0900

    親要素から子要素のスタイルを指定

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -12,23 +12,9 @@ const EditButton = ({ onClick }) => {
   );
 };
 
-const StyledBtn = styled.button`
-  position: relative;
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  border: none;
-  background-color: transparent;
-`;
-
-const Img = styled.img`
-  position: absolute;
-  width: 15px;
-  height: 15px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
+// HoverCircle→StyledBtnの順番でないと
+// 親要素から子要素のスタイルを指定できないため以下の順番になっています。
+// 参考(https://github.com/styled-components/styled-components/issues/3372#issuecomment-758119914)
 
 const HoverCircle = styled.div`
   position: absolute;
@@ -39,10 +25,31 @@ const HoverCircle = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: transparent;
-  ${StyledBtn}:hover > & {
-    background-color: ${COLOR.LIGHT_GRAY};
-    opacity: 0.2;
+`;
+
+const StyledBtn = styled.button`
+  position: relative;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: none;
+  background-color: transparent;
+
+  &:hover > ${HoverCircle} {
+      background-color: ${COLOR.LIGHT_GRAY};
+      opacity: 0.2;
+    }
   }
+
+`;
+
+const Img = styled.img`
+  position: absolute;
+  width: 15px;
+  height: 15px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 export default EditButton;
